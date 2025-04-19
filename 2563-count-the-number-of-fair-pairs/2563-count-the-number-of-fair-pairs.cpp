dccb22e76b7b7e8bba5b601e4d6f965999a00f98
BIKASH KUMAR YADAV <93582708+034-BikashKumarYadav@users.noreply.github.com>
2025-04-19 23:44:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,58 +1,10 @@
-#define ll long long
 class Solution {
-public:
-    long long countFairPairs(vector<int>& a, int low, int upp) {
-        ll ans=0;
-        //sort the vector
-        sort(a.begin(),a.end());
-        int n=a.size();
-
-        //iterate over each element
-        for(int i=0;i<n;i++)
-        {
-            int l=i+1,h=n-1;
-
-            //f and s will store the last and first index that form 
-            //fair pair with current index ,respectively
-            int f=-1,s=-1;
-            while(l<=h)
-            {
-                int m=l+h;
-                m>>=1;
-                if(low<=a[i]+a[m] && a[i]+a[m]<=upp)
-                {
-                    f=m;
-                    l=m+1;
-                }
-                else if(low>a[i]+a[m])
-                {
-                    l=m+1;
-                }
-                else h=m-1;
-            }
-
-            l=i+1,h=n-1;
-            while(l<=h)
-            {
-                int m=l+h;
-                m>>=1;
-                if(low<=a[i]+a[m] && a[i]+a[m]<=upp)
-                {
-                    s=m;
-                    h=m-1;
-                }
-                else if(low>a[i]+a[m])
-                {
-                    l=m+1;
-                }
-                else h=m-1;
-            }
-
-            if(f!=-1)
-            ans=ans+(f-s+1);
-
+    public:
+        long long countFairPairs(vector<int>& nums, int lower, int upper, long long ans = 0) {
+            for(int i = 0 ; i < nums.size(); i++)
+                for(int j = i + 1; j < nums.size(); j++)
+                    if(nums[i] + nums[j] >= lower && nums[i] + nums[j] <= upper) ++ans;
+    
+            return ans;
         }
-
-        return ans;
-    }
-};
+    };
