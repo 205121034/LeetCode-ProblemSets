eb49a583a4b6ee5b22c551c0730ece8f63cdf0e9
BIKASH KUMAR YADAV <93582708+bikashkumaryadav@users.noreply.github.com>
2025-07-28 17:48:56 +0530

    Time: 102 ms (20.53%), Space: 75.9 MB (17.75%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,33 +1,17 @@
-class Solution
-{
+class Solution {
 public:
-    void backtrack(const vector<int> &nums, int index, int currentOR, int maxOR, int &count)
-    {
-        if (currentOR == maxOR)
-        {
-            count++;
+    int countMaxOrSubsets(vector<int>& nums) {
+        vector<int> dp(1 << 17, 0);
+        dp[0] = 1;
+        int maxOr = 0;
+
+        for (int num : nums) {
+            for (int i = maxOr; i >= 0; --i) {
+                dp[i | num] += dp[i];
+            }
+            maxOr |= num;
         }
 
-        for (int i = index; i < nums.size(); ++i)
-        {
-            backtrack(nums, i + 1, currentOR | nums[i], maxOR, count);
-        }
-    }
-
-    int countMaxOrSubsets(vector<int> &nums)
-    {
-        int maxOR = 0;
-
-        // Step 1: Compute the maximum OR
-        for (int num : nums)
-        {
-            maxOR |= num;
-        }
-
-        int count = 0;
-        // Step 2: Backtrack to count the subsets
-        backtrack(nums, 0, 0, maxOR, count);
-
-        return count;
+        return dp[maxOr];
     }
 };
